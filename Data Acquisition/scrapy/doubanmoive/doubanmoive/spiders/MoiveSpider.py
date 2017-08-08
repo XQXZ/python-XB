03464497c1453c5d02a5266b20330f8042125e45
Soleil <30571564+Soleil000@users.noreply.github.com>
2017-08-08 21:29:28 +0800

    Add files via upload

--- Data Acquisition/scrapy/doubanmoive/doubanmoive/spiders/MoiveSpider.py
+++ Data Acquisition/scrapy/doubanmoive/doubanmoive/spiders/MoiveSpider.py
@@ -1,31 +1,38 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy.spiders import Spider
+from scrapy.http import Request
+from scrapy.selector import Selector
 from doubanmoive.items import DoubanmoiveItem
 
-class MoivespiderSpider(scrapy.Spider):
+class MoivespiderSpider(Spider):
     name = 'MoiveSpider'
     start_urls = ['https://movie.douban.com/top250']
 
     def parse(self, response):
-        item = DoubanmoiveItem()
-        for moive in response.xpath('//div[@class="info"]'):
+        
+        selector = Selector(response)
+        movies = selector.xpath('//div[@class="info"]')
+        for moive in movies:
+            item = DoubanmoiveItem()
             title = moive.xpath('div[@class="hd"]/a/span/text()').extract()
             complete_title = ''
             for each in title:
                 complete_title += each
-            info = movie.xpath('div[@class="bd"]/p/text()').extract()
+            minfo = movie.xpath('div[@class="bd"]/p/text()').extract()
             star = moive.xpath('div[@class="bd"]/div[@class="star"]/span[@class="rating_num"]/text()').extract()[0]
             quote = moive.xpath('div[@class="bd"]/p/span/text()').extract()[0]
             if not quote:
                 quote = ''
             item['title'] = complete_title;
-            item['info'] = ' '.join(info).replace(' ', '').replace('\n', '')
+            item['info'] = ' '.join(minfo).replace(' ', '').replace('\n', '')
             item['star'] = star[0]
             item['quote'] = quote[0]
             yield item
         nextPage = response.xpath('//span[@class="next"]/link/@href').extract()
         if nextPage:
             nextPage = nextPage[0]  
-            yield scrapy.Request(self.url + str(nextPage), callback=self.parse)
+            yield Request(self.url + str(nextPage), callback=self.parse)
+        
+        
             
                 
